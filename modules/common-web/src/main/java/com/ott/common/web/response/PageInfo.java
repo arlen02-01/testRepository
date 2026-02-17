ba45e727f8750b041c738b904d31a622a73c7f14
강승우 <phonil@naver.com>
2026-02-17 21:52:12 +0900

    [CHORE]: 빈 생성자 롬복 어노테이션으로 수정

--- modules/common-web/src/main/java/com/ott/common/web/response/PageInfo.java
+++ modules/common-web/src/main/java/com/ott/common/web/response/PageInfo.java
@@ -3,8 +3,10 @@ package com.ott.common.web.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@NoArgsConstructor
 @ToString
 @Getter
 public class PageInfo {
@@ -17,8 +19,6 @@ public class PageInfo {
 
     @Schema(type = "Integer", example = "10", description = "한 페이지의 사이즈")
     private Integer pageSize;
-
-    public PageInfo(){};
 
     @Builder
     public PageInfo(Integer currentPage, Integer totalPage, Integer pageSize) {
